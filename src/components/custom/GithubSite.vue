37b8f93403a46e1b9d1ee248c956a65c138811d1
ZeoXel <likele.zoom@gmail.com>
2025-08-12 16:36:26 +0800

    添加 Vidu 前

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
+    <span> </span>
     <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+      API 获取
     </a>
   </div>
 </template>
