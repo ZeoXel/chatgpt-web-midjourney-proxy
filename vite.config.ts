10f638cf7281008765d936aa38225b6ff1d15084
ZeoXel <likele.zoom@gmail.com>
2025-08-15 11:19:05 +0800

    进行“服务端”更改前

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ function setupPlugins(env: ImportMetaEnv): PluginOption[] {
     viteStaticCopy({
       targets: [
         {
-          src: path.resolve(__dirname, './src/static/mitf') + '/[!.]*', // 1️⃣
+          src: `${path.resolve(__dirname, './src/static/mitf')}/[!.]*`, // 1️⃣
           dest: './mitf/', // 2️⃣
         },
       ],
@@ -41,8 +41,8 @@ export default defineConfig((env) => {
     },
     plugins: setupPlugins(viteEnv),
     server: {
-      host: '0.0.0.0',
-      port: 1002,
+      host: 'localhost',
+      port: 3000,
       open: false,
       proxy: {
         '/api': {
@@ -53,39 +53,39 @@ export default defineConfig((env) => {
         '/mjapi': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
-          //rewrite: path => path.replace('/api/', '/'),
+          // rewrite: path => path.replace('/api/', '/'),
         },
-         '/sunoapi': {
-          target: viteEnv.VITE_APP_API_BASE_URL,
-          changeOrigin: true, // 允许跨域  
-        },
-         '/uploads': {
+        '/sunoapi': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
-          //rewrite: path => path.replace('/api/', '/'),
-        }, 
+        },
+        '/uploads': {
+          target: viteEnv.VITE_APP_API_BASE_URL,
+          changeOrigin: true, // 允许跨域
+          // rewrite: path => path.replace('/api/', '/'),
+        },
         '/openapi': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
-          //rewrite: path => path.replace('/api/', '/'),
+          // rewrite: path => path.replace('/api/', '/'),
         },
         '/luma': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
-          //rewrite: path => path.replace('/api/', '/'),
-        }, 
+          // rewrite: path => path.replace('/api/', '/'),
+        },
         //
         '/viggle': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
-          //rewrite: path => path.replace('/api/', '/'),
+          // rewrite: path => path.replace('/api/', '/'),
         },
-         '/runwayml': {
+        '/runwayml': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
-          //rewrite: path => path.replace('/api/', '/'),
+          // rewrite: path => path.replace('/api/', '/'),
         },
-        
+
       },
     },
     build: {
